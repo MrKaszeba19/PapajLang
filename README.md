6452657550094cd889466c9c1081174e3d93250a
RooiGevaar19 <redthreat19@gmail.com>
2017-11-24 14:28:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# RPN Calculator
+Reversed Polish Notation Calculator
+Version 0.1.0
+November 24, 2017
+by *Paul Lipkowski* (RooiGevaar19)
+
+> Pascal is not dead. It's just a Force that remains forever, just like it does in Luke Skywalker. 
+
+## About
+**Reverse Polish Notation** (RPN) is a mathematical notation in which operators follow their operands. It allows to evaluate the mathematical expressions without using parentheses.
+
+**More info:** https://en.wikipedia.org/wiki/Reverse_Polish_notation
+
+## Examples of RPN expressions
+
+ Ordinary expression             | RPN Expression
+---------------------------------+---------------------------
+ 5                               | 5
+ 2 + 3                           | 2 3 +
+ (2.5 * 2) + 5                   | 2.5 2 * 5 +
+ -4 * (2 / 3)                    | -4 2 3 / *
+ (2 + 5) * (11 - 7)              | 2 5 + 11 7 - *
+ ((8 - 2) / 3 + (1 + 4) * 2) / 6 | 8 2 - 3 / 1 4 + 2 * + 6 /
+
+## How to use it
+In order to solve an RPN expression, just type it in the upper text box and click the "Count it!"-button. The result appears in the result box below. Remember that all values and operands must be delimited with 1 space char.
+
+Possible operations:
+- add (+)
+- substract (-)
+- multiply (*)
+- divide (/)
+- power (^)
+- root (root)
+- logarithm (log)
+
+Possible constants:
+- Pi "π" (PI)
+- Euler number "e" (EU)
+- Golden number "φ" (FI)
+
+## Languages
+- **English** (*default*)
+
---
+++ rpn_kernafval/unit2.pas
@@ -0,0 +1,184 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+const
+        PI = 3.1415926535897932384626433832795;
+        EU = 2.7182818284590;
+        FI = 1.618033989;
+
+type PStos = ^TStos;
+     TStos    = record
+     Liczba   : extended;
+     Nastepny : PStos;
+end;
+
+function pow(x,y:extended):extended;
+procedure calc_stack_add(var pocz:PStos; number:extended);
+function calc_stack_show(pocz:PStos) : extended;
+procedure calc_stack_remove(var pocz:PStos);
+function calc_parseRPN(input : string) : extended;
+
+implementation
+uses Unit1;
+
+function pow(x,y:extended):extended;
+var
+        s : real;
+begin
+        s := exp(y*ln(x));
+        pow := s;
+end;
+
+procedure calc_stack_add(var pocz:PStos; number:extended);
+var
+    Nowy: PStos;
+  begin
+    New(Nowy);
+    Nowy^.Liczba := number;
+    Nowy^.Nastepny := Pocz;
+    Pocz := Nowy;
+  end;
+
+
+function calc_stack_show(pocz:PStos) : extended;
+begin
+        calc_stack_show := pocz^.Liczba;
+end;
+
+procedure calc_stack_remove(var pocz:PStos);
+var
+    Pom: PStos;
+  begin
+    //while Pocz <> nil do
+    //begin
+      Pom := Pocz^.Nastepny;
+      Dispose(Pocz);
+      Pocz := Pom;
+    //end;
+  end;
+
+function calc_parseRPN(input : string) : extended;
+var
+        x, y, z        : extended;
+        pocz           : PStos;
+        L              : TStrings;
+        i              : String;
+        Im             : Extended;
+        Code           : Integer;
+begin
+        // delimites string
+        //https://forum.lazarus.freepascal.org/index.php?topic=33644.0
+        L := TStringlist.Create;
+        L.Delimiter := ' ';
+        L.QuoteChar := '"';
+        L.StrictDelimiter := false;  // set this to false and the second 'test me' will be separate items! Try it.
+        L.DelimitedText := input;
+
+        pocz := nil;
+        for i in L do
+        begin
+             Val (i,Im,Code);
+             If Code<>0 then
+                //Writeln ('Error at position ',code,' : ',Paramstr(1)[Code])
+                begin
+                     case i of
+                     '+' : begin
+                         y := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         x := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         z := x+y;
+                         calc_stack_add(pocz, z);
+                         continue;
+                     end;
+                     '-' : begin
+                         y := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         x := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         z := x-y;
+                         calc_stack_add(pocz, z);
+                         continue;
+                     end;
+                     '*' : begin
+                         y := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         x := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         z := x*y;
+                         calc_stack_add(pocz, z);
+                         continue;
+                     end;
+                     '/' : begin
+                         y := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         x := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         z := x/y;
+                         calc_stack_add(pocz, z);
+                         continue;
+                     end;
+                     '^' : begin
+                         y := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         x := pocz^.Liczba;
+                         calc_stack_remove(pocz);
+                         z := pow(x,y);
+                         calc_stack_add(pocz, z);
+                         continue;
+                     end;
+                     'root' : begin
+                            y := pocz^.Liczba;
+                            calc_stack_remove(pocz);
+                            x := pocz^.Liczba;
+                            calc_stack_remove(pocz);
+                            z := pow(x,1/y);
+                            calc_stack_add(pocz, z);
+                            continue;
+                     end;
+                     'log' : begin
+                           y := pocz^.Liczba;
+                           calc_stack_remove(pocz);
+                           x := pocz^.Liczba;
+                           calc_stack_remove(pocz);
+                           z := ln(y)/ln(z);
+                           calc_stack_add(pocz, z);
+                           continue;
+                     end;
+                     'PI' : begin
+                           calc_stack_add(pocz, PI);
+                           continue;
+                     end;
+                     'EU' : begin
+                           calc_stack_add(pocz, EU);
+                           continue;
+                     end;
+                     'FI' : begin
+                           calc_stack_add(pocz, FI);
+                           continue;
+                     end;
+                     end;
+             end else begin
+                 begin
+                      calc_stack_add(pocz, Im);
+                 end;
+             end;
+
+        end;
+        z := pocz^.Liczba;
+
+        while pocz <> nil do calc_stack_remove(pocz);
+        L.Free;
+
+        calc_parseRPN := z;
+end;
+
+
+end.
+
