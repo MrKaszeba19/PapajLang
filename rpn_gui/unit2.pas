0714e100814cd56a227b035a1947bba4d1b4cd41
RooiGevaar19 <redthreat19@gmail.com>
2018-11-08 08:59:23 +0100

    Remove some entries from history

--- rpn_gui/unit2.pas
+++ rpn_gui/unit2.pas
@@ -211,8 +211,10 @@ begin
     writeln('Type \reset to reset the REPL.');
     writeln('Type \export:FILE to export your history to a file (relative or absolute path)');
     writeln('Type \history to display what you did.');
+    writeln('Type \hclear to clear all history');
+    writeln('Type \hclear:N to the N-th command of history (N >= 1)');
     writeln('Type \!! to repeat last command');
-    writeln('Type \!N to execute an N-th command of history (N >= 1)');
+    writeln('Type \!N to execute the N-th command of history (N >= 1)');
     writeln('Type \help to display this help again.');
     writeln('Type \q or \quit to exit the REPL.');
     writeln();
@@ -279,6 +281,21 @@ begin
             if Code = 0 then
                 if (Trunc(Im) >= 1) and (Trunc(Im) <= Length(history)) then
                     input := history[Trunc(Im)-1]; 
+        end else if LeftStr(input, 8) = '\hclear:' then
+        begin
+            Val(RightStr(input, Length(input)-8), Im, Code);
+            if Code = 0 then
+                if (Trunc(Im) >= 1) and (Trunc(Im) <= Length(history)) then
+                begin
+                    fname := history[Trunc(Im)-1];
+                    for i := Trunc(Im)-1 to Length(history)-2 do
+                        history[i] := history[i+1];
+                    SetLength(history, Length(history)-1);
+                    TextColor(10);
+                    writeln('Entry #',RightStr(input, Length(input)-8),' of ',QuotedStr(fname),' has been removed successfully.');
+                    TextColor(7);
+                end;
+            input := '';
         end else if (LeftStr(input, 8) = '\export:') or (input = '\export') then
         begin
             if (input <> '\export') then fname := RightStr(input, Length(input)-8)
@@ -316,6 +333,13 @@ begin
             end;
             '\quit' : begin
                 writeln('Goodbye. :)');
+            end;
+            '\hclear' : begin
+                SetLength(history, 0);
+                TextColor(10);
+                writeln('Your history is empty now.');
+                writeln();
+                TextColor(7);
             end;
             '\comamrobic' : begin
                 randomize;
