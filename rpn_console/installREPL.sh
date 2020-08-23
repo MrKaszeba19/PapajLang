1eca5d1908639c1e97564bdf09d354327ce92a3a
RooiGevaar19 <redthreat19@gmail.com>
2020-08-23 20:46:21 +0200

    some scripts for GUI app

--- rpn_console/installREPL.sh
+++ rpn_console/installREPL.sh
@@ -1,15 +1,25 @@
+#!/bin/bash
 echo Starting...
-path=`xdg-user-dir DESKTOP`
+if ! command -v xdg-user-dir &> /dev/null then
+then
+    echo "Warning: xdg-user-dir not installed on this computer."
+    echo "Using explicit ~/Desktop as the Desktop path. This path might not exist on non-English OSes."
+    path=~/Desktop
+else
+    path=`xdg-user-dir DESKTOP`
+fi
 echo "#!/usr/bin/env xdg-open" > $path/RPN.desktop
 echo "[Desktop Entry]" >> $path/RPN.desktop
 echo "Version=0.5" >> $path/RPN.desktop
 echo "Type=Application" >> $path/RPN.desktop
 echo "Terminal=true" >> $path/RPN.desktop
 echo "Exec=$(pwd)/rpn repl" >> $path/RPN.desktop
-echo "Name=RPN REPL 0.5.0" >> $path/RPN.desktop
+echo "Name=RPN REPL" >> $path/RPN.desktop
 echo "Comment=REPL of RPN Calculator – PapajScript interpreter." >> $path/RPN.desktop
 echo "Icon=$(pwd)/rpn.ico" >> $path/RPN.desktop
 if [ -f $path/RPN.desktop ] ; then
 	chmod +x ~/Desktop/RPN.desktop
-	echo The shortcut of RPN REPL has been created on Desktop.
+	echo "The shortcut of RPN REPL has been created at $path."
+else
+    echo "Error: cannot create a shortcut at $path."
 fi
---
+++ rpn_gui/installShortcut.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo Starting...
+if ! command -v xdg-user-dir &> /dev/null then
+then
+    echo "Warning: xdg-user-dir not installed on this computer."
+    echo "Using explicit ~/Desktop as the Desktop path. This path might not exist on non-English OSes."
+    path=~/Desktop
+else
+    path=`xdg-user-dir DESKTOP`
+fi
+echo "#!/usr/bin/env xdg-open" > $path/RPNG.desktop
+echo "[Desktop Entry]" >> $path/RPNG.desktop
+echo "Version=0.5" >> $path/RPNG.desktop
+echo "Type=Application" >> $path/RPNG.desktop
+echo "Exec=$(pwd)/rpncalculator" >> $path/RPNG.desktop
+echo "Name=RPN Calculator GUI" >> $path/RPNG.desktop
+echo "Comment=A GUI app of RPN Calculator – PapajScript interpreter." >> $path/RPNG.desktop
+echo "Icon=$(pwd)/rpncalculator.ico" >> $path/RPNG.desktop
+if [ -f $path/RPNG.desktop ] ; then
+	chmod +x ~/Desktop/RPNG.desktop
+	echo "The shortcut of RPN Calculator GUI has been created at $path."
+else
+    echo "Error: cannot create a shortcut at $path."
+fi
