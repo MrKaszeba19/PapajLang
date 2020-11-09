d0c8860e301ad17684dfe1aca3539fac0805e2fd
RooiGevaar19 <redthreat19@gmail.com>
2020-11-09 21:42:14 +0100

    Zala's discovery of installREPL path bug <3

--- rpn_console/installREPL.sh
+++ rpn_console/installREPL.sh
@@ -10,18 +10,18 @@ else
     path=`xdg-user-dir DESKTOP`
 fi
 
-echo "#!/usr/bin/env xdg-open" > $path/RPN.desktop
-echo "[Desktop Entry]" >> $path/RPN.desktop
-echo "Version=0.5" >> $path/RPN.desktop
-echo "Type=Application" >> $path/RPN.desktop
-echo "Terminal=true" >> $path/RPN.desktop
-echo "Exec=$(pwd)/rpn repl" >> $path/RPN.desktop
-echo "Name=RPN REPL" >> $path/RPN.desktop
-echo "Comment=REPL of RPN Calculator – PapajScript interpreter." >> $path/RPN.desktop
-echo "Icon=$(pwd)/rpn.ico" >> $path/RPN.desktop
+echo "#!/usr/bin/env xdg-open" > "$path/RPN.desktop"
+echo "[Desktop Entry]" >> "$path/RPN.desktop"
+echo "Version=0.5" >> "$path/RPN.desktop"
+echo "Type=Application" >> "$path/RPN.desktop"
+echo "Terminal=true" >> "$path/RPN.desktop"
+echo -e "Exec=\"$(pwd)/rpn\" repl" >> "$path/RPN.desktop"
+echo "Name=RPN REPL" >> "$path/RPN.desktop"
+echo "Comment=REPL of RPN Calculator – PapajScript interpreter." >> "$path/RPN.desktop"
+echo "Icon=$(pwd)/rpn.ico" >> "$path/RPN.desktop"
 
-if [ -f $path/RPN.desktop ] ; then
-	chmod +x $path/RPN.desktop
+if [ -f "$path/RPN.desktop" ] ; then
+	chmod +x "$path/RPN.desktop"
 	echo "The shortcut of RPN REPL has been created at $path."
 else
     echo "Error: cannot create a shortcut at $path."
--- rpn_gui/installShortcut.sh
+++ rpn_gui/installShortcut.sh
@@ -8,16 +8,16 @@ then
 else
     path=`xdg-user-dir DESKTOP`
 fi
-echo "#!/usr/bin/env xdg-open" > $path/RPNG.desktop
-echo "[Desktop Entry]" >> $path/RPNG.desktop
-echo "Version=0.5" >> $path/RPNG.desktop
-echo "Type=Application" >> $path/RPNG.desktop
-echo "Exec=$(pwd)/rpncalculator" >> $path/RPNG.desktop
-echo "Name=RPN Calculator GUI" >> $path/RPNG.desktop
-echo "Comment=A GUI app of RPN Calculator – PapajScript interpreter." >> $path/RPNG.desktop
-echo "Icon=$(pwd)/rpncalculator.ico" >> $path/RPNG.desktop
-if [ -f $path/RPNG.desktop ] ; then
-	chmod +x ~/Desktop/RPNG.desktop
+echo "#!/usr/bin/env xdg-open" > "$path/RPNG.desktop"
+echo "[Desktop Entry]" >> "$path/RPNG.desktop"
+echo "Version=0.5" >> "$path/RPNG.desktop"
+echo "Type=Application" >> "$path/RPNG.desktop"
+echo -e "Exec=\"$(pwd)/rpncalculator\"" >> "$path/RPNG.desktop"
+echo "Name=RPN Calculator GUI" >> "$path/RPNG.desktop"
+echo "Comment=A GUI app of RPN Calculator – PapajScript interpreter." >> "$path/RPNG.desktop"
+echo "Icon=$(pwd)/rpncalculator.ico" >> "$path/RPNG.desktop"
+if [ -f "$path/RPNG.desktop" ] ; then
+	chmod +x "$path/RPNG.desktop"
 	echo "The shortcut of RPN Calculator GUI has been created at $path."
 else
     echo "Error: cannot create a shortcut at $path."
