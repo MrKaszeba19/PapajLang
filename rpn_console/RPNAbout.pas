e8458642bb0be36a7b3a9f5809893ce8e7d5f2ab
RooiGevaar19 <redthreat19@gmail.com>
2023-11-26 20:55:08 +0100

    Edit version number

--- rpn_console/RPNAbout.pas
+++ rpn_console/RPNAbout.pas
@@ -3,13 +3,13 @@ unit RPNAbout;
 interface
 
 const RPN_version = '0.5.3';
-const RPN_update = 1;
+const RPN_update = 2;
 const RPN_codename = 'Mem';
 const RPN_generation = 3;
 const RPN_isStable = True;
 //const RPN_date = {$I %DATE%};
 const RPN_date = '2023-04-08';
-const RPN_updated = '2023-04-11';
+const RPN_updated = '2023-11-26';
 const RPN_targetCPU = {$I %FPCTARGETCPU%};
 {$ifdef WINDOWS}
 const RPN_targetOS = 'Windows';
